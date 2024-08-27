40c5d2c81d360a6ba81e1bf68e23b5d5daeb3e35
Shree Dave <shreedave0@gmail.com>
2024-08-27 21:46:22 +0530

    show incident markers on map

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import pg from "pg";
 
 const app = express();
 
@@ -9,16 +10,60 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("status.ejs", { apiKey: apiKey });
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: "mseb",
+  password: process.env.DB_PASSWORD,
+  port: 5432,
 });
 
-app.post("/report", (req, res) => {
+db.connect();
+
+app.get("/", async (req, res) => {
+  try {
+    const result = (
+      await db.query(
+        "select t1.incident_type, t1.reported_on, consumers.id, consumers.meter_lat, consumers.meter_lng from incidents t1 join consumers on t1.consumer_id = consumers.id where reported_on = (select max(reported_on) from incidents where t1.consumer_id = incidents.consumer_id) order by reported_on desc"
+      )
+    ).rows;
+    console.log(result);
+    res.render("status.ejs", { apiKey: apiKey, result: result });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/error");
+  }
+});
+
+app.get("/error", (req, res) => {
+  res.send("ERROR");
+});
+
+app.post("/report", async (req, res) => {
   if (req.headers.authorization.split()[1] === process.env.APP_TOKEN) {
     console.log(req.body);
-    res.status(200).json({ success: "Message received" });
-  } else{
-    res.status(401).json({error: "Not Authorized"});
+    try {
+      let result = (
+        await db.query("select id from consumers where phone=$1", [
+          req.body.consumer_phone,
+        ])
+      ).rows;
+      if (result.length != 0) {
+        const consumerId = result[0].id;
+        await db.query(
+          "insert into incidents (incident_type, reported_on, consumer_id) values ($1, $2, $3)",
+          [req.body.incident_type, new Date(), consumerId]
+        );
+        res.status(200).json({ success: "Message received" });
+      } else {
+        res.status(404).json({ error: "Not found" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Technical Error" });
+    }
+  } else {
+    res.status(401).json({ error: "Not Authorized" });
   }
 });
 
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,17 +10,28 @@ async function initMap() {
     mapId: "MSEB_LAYOUT_MAP_ID",
   });
 
+  let incidents = document.getElementById("incidents");
+  incidents = JSON.parse(incidents.textContent);
+
+  for (let incident of incidents){
+    console.log(incident.meter_lat);
+    placeMarkerAndPanTo({lat: incident.meter_lat, lng: incident.meter_lng}, map, `Reported ${incident.incident_type} on ${incident.reported_on} by ${incident.id}`);
+  }
+  
   map.addListener("click", (e) => {
     placeMarkerAndPanTo(e.latLng, map);
   });
 }
 
-function placeMarkerAndPanTo(latLng, map) {
+function placeMarkerAndPanTo(latLng, map, title) {
   new google.maps.marker.AdvancedMarkerElement({
     position: latLng,
     map: map,
+    title: title,
   });
   map.panTo(latLng);
 }
 
 initMap();
+
+
