094e9448a60f1d2535c239c9d8bd71d5d20a288a
Shree Dave <shreedave0@gmail.com>
2024-08-27 19:56:05 +0530

    add app token check

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ const app = express();
 const port = process.env.port || 3000;
 const apiKey = process.env.GOOGLE_MAPS_APIKEY;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("status.ejs", {apiKey: apiKey});
+  res.render("status.ejs", { apiKey: apiKey });
 });
 
 app.post("/report", (req, res) => {
-    console.log(req.headers.authorization);
+  if (req.headers.authorization.split()[1] === process.env.APP_TOKEN) {
     console.log(req.body);
-    res.status(200).json({"success": "Message received"});
+    res.status(200).json({ success: "Message received" });
+  } else{
+    res.status(401).json({error: "Not Authorized"});
+  }
 });
-
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
-
