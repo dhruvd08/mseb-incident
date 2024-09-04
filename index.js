641d6dbcc0e3aa2c32691aea0cebcaed445e0ad8
Shree Dave <shreedave0@gmail.com>
2024-09-04 22:30:37 +0530

    add registration use case

--- index.js
+++ index.js
@@ -65,26 +65,26 @@ app.post("/notify-webhook", async (req, res) => {
               break;
           }
         } else {
-          if (msg.type === "text") {
-            const content = msg.text.body;
-            console.log(content);
-            if (isConsumerId(content)) {
-              console.log(`Got consumer number ${content}`);
-              //await consumer.addConsumer(content, sender.wa_id, );
-              await consumer.sendLocationReq(sender.wa_id);
-            } else {
-              await consumer.sendIncidentTypeSelection(sender.wa_id);
-            }
-          }
+          await consumer.sendIncidentTypeSelection(sender.wa_id);
         }
-      } else if (msg.type === "interactive") {
-        incident_type = msg.interactive.button_reply.id;
-        console.log(`Incident type ${incident_type}`);
       }
-      await incident.addIncident(incident_type, dbConsumer);
-      await consumer.sendAck(sender.wa_id);
-    } else {
-      await consumer.sendNoLinkedPhoneFoundMsg(sender.wa_id);
+    } else if (msg.type === "interactive") {
+      incident_type = msg.interactive.button_reply.id;
+      console.log(`Incident type ${incident_type}`);
+    }
+    await incident.addIncident(incident_type, dbConsumer);
+    await consumer.sendAck(sender.wa_id);
+  } else {
+    if (msg.type === "text") {
+      const content = msg.text.body;
+      console.log(content);
+      if (isConsumerId(content)) {
+        console.log(`Got consumer number ${content}`);
+        //await consumer.addConsumer(content, sender.wa_id, sender.profile.name);
+        await consumer.sendLocationReq(sender.wa_id);
+      } else {
+        await consumer.sendNoLinkedPhoneFoundMsg(sender.wa_id);
+      }
     }
   }
   res.sendStatus(200);
