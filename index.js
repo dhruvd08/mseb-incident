9dfba783d5d0e8400b9a0dd62a16569e41cb3193
Shree Dave <shreedave0@gmail.com>
2024-09-08 11:16:10 +0530

    change sse

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "eagle-5i6w.onrender.com");
-  res.header("Access-Control-Allow-Origin", "graph.facebook.com");
-  res.header("Access-Control-Allow-Origin", "localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://eagle-5i6w.onrender.com");
+  res.header("Access-Control-Allow-Origin", "https://graph.facebook.com");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
